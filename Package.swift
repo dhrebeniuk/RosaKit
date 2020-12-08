dc92ea4e70babdcbbaabe3482611e973a53ada1d
dhrebeniuk <dmytrohrebeniuk@gmail.com>
2020-12-08 12:12:01 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,10 @@
+import PackageDescription
+
+let package = Package(name: "RosaKit",
+                      platforms: [.iOS(.v8)],
+                      products: [.library(name: "RosaKit",
+                                          targets: ["RosaKit"])],
+                      targets: [.target(name: "RosaKit",
+                                        path: ".")],
+                      swiftLanguageVersions: [.v5])
+                      
