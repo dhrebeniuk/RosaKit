1236bb33ee2acf2d7a98370c86df82b97ebe1f3b
Dmytro Hrebeniuk <dmytrohrebeniuk@gmail.com>
2021-05-18 06:13:30 +0300

    Setup minium ios to 9.0

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(name: "RosaKit",
-                      platforms: [.iOS(.v8)],
+                      platforms: [.iOS(.v9)],
                       products: [.library(name: "RosaKit",
                                           targets: ["RosaKit"])],
                       dependencies: [
