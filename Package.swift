3ef5cc30d4a83e27b2090fef04471cb3a9cc80f6
dhrebeniuk <dmytrohrebeniuk@gmail.com>
2022-02-09 01:07:07 +0200

    Resolved build fail

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(name: "RosaKit",
                       products: [.library(name: "RosaKit",
                                           targets: ["RosaKit"])],
                       dependencies: [
-                        .package(url: "https://github.com/dhrebeniuk/plain-pocketfft.git".branch("main"))
+                        .package(url: "https://github.com/dhrebeniuk/plain-pocketfft.git", .branch("main"))
                       ], targets: [.target(name: "RosaKit", dependencies: ["PlainPocketFFT"],
                                            path: "Sources")],
                       swiftLanguageVersions: [.v5])
