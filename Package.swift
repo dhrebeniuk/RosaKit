a2c090198603fe0a825513b2312d26d61e5bb9b0
Dmytro Hrebeniuk <dmytrohrebeniuk@gmail.com>
2022-02-10 00:04:11 +0200

    Update SPM depencies

--- Package.swift
+++ Package.swift
@@ -7,8 +7,8 @@ let package = Package(name: "RosaKit",
                       products: [.library(name: "RosaKit",
                                           targets: ["RosaKit"])],
                       dependencies: [
-                        .package(url: "https://github.com/dhrebeniuk/plain-pocketfft.git", .branch("main")),
-                        .package(url: "https://github.com/dhrebeniuk/pocketfft.git", .branch("main")),
+                        .package(url: "https://github.com/dhrebeniuk/plain-pocketfft.git", from: "0.0.9"),
+                        .package(url: "https://github.com/dhrebeniuk/pocketfft.git", from: "0.0.1"),
                       ], targets: [.target(name: "RosaKit", dependencies: ["PlainPocketFFT", "PocketFFT"],
                                            path: "Sources")],
                       swiftLanguageVersions: [.v5])
